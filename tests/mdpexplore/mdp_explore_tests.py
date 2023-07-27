077f64b77dcc33e84429f08fdde66edbb3600f77
Jose Pablo Folch <jpfolch@outlook.com>
2023-07-27 15:42:01 +0200

    added density oracle unit test

---
+++ tests/mdpexplore/mdp_explore_tests.py
@@ -0,0 +1,132 @@
+import numpy as np
+from mdpexplore.solvers.dp import DP
+from mdpexplore.env.env_dummy_testing import DummyTestEnv
+from mdpexplore.mdpexplore import MdpExplore
+from mdpexplore.functionals.doe_adaptive_functionals import AdaptiveDesignD
+
+from mdpexplore.policies.stationary_policy import StationaryPolicy
+from mdpexplore.policies.non_stationary_policy import NonStationaryPolicy
+
+import pytest
+
+env = DummyTestEnv()
+design = AdaptiveDesignD(env)
+
+
+me = MdpExplore(
+    env,
+    objective=design,
+    solver=DP,
+    method='frank-wolfe',
+    verbosity=0)
+
+@pytest.mark.parametrize("policy_type", ['StationaryPolicy', 'NonStationaryPolicy'])
+def test_density_oracle_single(policy_type : str):
+    # test that the density oracle returns the correct density
+    p = np.array([[0., 1., 0., 0., 0.],
+                [0., 0., 1., 0., 0.],
+                [0., 0., 0., 1., 0.],
+                [0., 0., 0., 0., 1.],
+                [0., 0., 0., 0., 1.]])
+
+    if policy_type == 'StationaryPolicy':
+        # test for stationary policy
+        policy = StationaryPolicy(env, p)
+        density = me._density_oracle_single(policy)
+
+        # make sure the state-action density is valid
+        for s in range(env.states_num):
+            for a in range(env.actions_num):
+                if density[s, a] > 0:
+                    assert env.is_valid_action(a, s), 'policy breaks environment constraints'
+                    assert density[s, a] <= 1, 'policy suggests a probability greater than 1'
+                else:
+                    density[s, a] == 0, 'policy suggests a negative probability'
+    
+        # test that the density is correct
+        correct_density = np.array([[0., 1., 0., 0., 0.],
+                                    [0., 0., 1., 0., 0.],
+                                    [0., 0., 0., 1., 0.],
+                                    [0., 0., 0., 0., 1.],
+                                    [0., 0., 0., 0., 7.]])
+        correct_density = correct_density / 11
+        assert np.all(density == correct_density), 'density oracle returns incorrect density'
+    
+    elif policy_type == 'NonStationaryPolicy':
+        ps = np.expand_dims(p, axis=0).repeat(10, axis=0)
+        # test for non-stationary policy
+        policy = NonStationaryPolicy(env, ps)
+        density = me._density_oracle_single(policy)
+
+        # test that the density is valid
+        for h in range(10):
+            for s in range(env.states_num):
+                for a in range(env.actions_num):
+                    if density[h, s, a] > 0:
+                        assert env.is_valid_action(a, s), 'policy breaks environment constraints'
+                        assert density[h, s, a] <= 1, 'policy suggests a probability greater than 1'
+                    else:
+                        density[h, s, a] == 0, 'policy suggests a negative probability'
+        
+        # test that the density is correct
+        correct_density = np.array([[[0., 1., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 1., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 1., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 1.],
+                                    [0., 0., 0., 0., 0.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 1.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 1.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 1.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 1.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 1.]],
+                                    
+                                    [[0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 0.],
+                                    [0., 0., 0., 0., 1.]]])
+
+        assert np.all(density == correct_density), 'density oracle returns incorrect density'
--- tests/solvers/dp_test.py
+++ tests/solvers/dp_test.py
@@ -1,56 +1,6 @@
 import numpy as np
 from mdpexplore.solvers.dp import DP
-from mdpexplore.env.discrete_env import DiscreteEnv
-
-class DummyTestEnv(DiscreteEnv):
-    def __init__(self, states_num = 5, actions_num = 5, max_episode_length = 10, constrained = False):
-        super().__init__(init_state=0)
-        self.states_num = states_num
-        self.actions_num = actions_num
-        self.max_episode_length = max_episode_length
-        self.terminal_state = None
-        self.constrained = constrained
-
-    def available_actions(self, state):
-        return [a for a in range(self.actions_num) if self.is_valid_action(a, state)]
-
-    def next(self, state, action):
-        return action
-    
-    def convert(self, state):
-        pass
-
-    def step(self, action: int):
-        self.visitations[action] += 1
-        return action
-
-    def get_transition_matrix(self) -> np.ndarray:
-        tm = np.zeros((self.states_num, self.actions_num, self.states_num))
-        for s in range(self.states_num):
-            for a in range(self.states_num):
-                if self.is_valid_action(a, s):
-                    tm[s, a, a] = 1.
-        return tm
-
-    def is_valid_action(self, action, state) -> bool:
-        if state == 0:
-            return action in [0, 1]
-            
-        if state == 1:
-            return action in [0, 1, 2]
-
-        
-        if state == 2:
-            return action in [1, 2, 3]
-        
-        if state == 3:
-            return action in [2, 3, 4]
-        
-        if state == 4:
-            return action in [3, 4]
-
-    def reset(self) -> None:
-        self.state = self.init_state
+from mdpexplore.env.env_dummy_testing import DummyTestEnv
 
 # initialize reward
 reward = np.zeros((10, 5, 5))
