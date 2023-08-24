30c46234012dfbc96fc09898827eb90d5aef8dfe
Jose Pablo Folch <jpfolch@outlook.com>
2023-08-24 18:13:35 +0200

    added ddpg solver with tests

---
+++ mdpexplore/utils/nn.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim=128):
+        super(MLP, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        return self.fc3(x)
