a31410139fdc505fdd874a1bac28191476062349
Mojusko <mojmir.mutny@inf.ethz.ch>
2024-10-30 16:47:56 +0100

    llms environment

---
+++ doexpy/env/llm.py
@@ -0,0 +1,145 @@
+from doexpy.env.discrete_env import DiscreteEnv
+from transformers import CLIPModel, CLIPProcessor, CLIPTokenizer
+from typing import List
+import torch
+class LLMGrid(DiscreteEnv):
+	def __init__(
+		self,
+        list_of_text_tokens: List[str] = None,
+		device: str = 'cpu',
+		MODELS_CACHE_DIR: str = '/tmp/models_cache_dir/',
+		verbose: bool = False,
+	):
+		self.verbose = verbose
+		self.device = device
+		self.constrained = False
+		super().__init__(
+			init_state=0,
+		)
+
+		self.list_of_text_tokens = list_of_text_tokens
+		self.max_episode_length = len(list_of_text_tokens)
+
+		# collapse the text tokens into a single list
+		self.tokens = {}
+
+		index = 1
+		self.tokens[' '] = [i for i in range(self.max_episode_length)]
+		self.unique_elements = [' ']
+		for order, list in enumerate(list_of_text_tokens):
+			for token in list:	
+				if token not in self.tokens:
+					self.unique_elements.append(token)
+					self.tokens[token] = [order]
+					index += 1
+				else:
+					self.tokens[token] += [order]
+			
+
+		# get the total number of states
+		total_tokens = len(self.unique_elements)
+
+		# create a LLM embedding model CLIP
+		clip_model_id = "openai/clip-vit-large-patch14"  # All stable diffusion models rely on VIT-14
+		self._tokenizer = CLIPTokenizer.from_pretrained(clip_model_id, cache_dir=str(MODELS_CACHE_DIR))
+		self._processor = CLIPProcessor.from_pretrained(clip_model_id, cache_dir=str(MODELS_CACHE_DIR))
+		self._model = CLIPModel.from_pretrained(clip_model_id, cache_dir=str(MODELS_CACHE_DIR)).to(self.device)
+
+		##
+		self.states_num = self.max_episode_length
+		self.actions_num = total_tokens
+		self.h = 0 
+		self.emiss_num = self.actions_num 
+		self.transition_matrix = None
+		self._generate_emissions()
+		self.visitations = torch.zeros(self.states_num, self.actions_num, dtype=torch.float64)
+		#print (self.max_episode_length)
+
+	def _generate_emissions(self):
+		if self.verbose:
+			print ("PREPROCESS: Generating emissions")
+		self.emissions = []
+		for i in range(self.actions_num):
+			
+			text = self.unique_elements[i]
+			text_input = self._tokenizer(
+				text,
+				padding="max_length",
+				max_length=self._tokenizer.model_max_length,
+				truncation=True,
+				return_tensors="pt",
+			)
+			if self.verbose:
+				print (f"Generating emission for action {i}, text: {text}")
+			feat = self._model.get_text_features(**text_input)  # projected CLIP embeddings
+			feat = feat.detach().double()
+			self.emissions.append(feat)
+		if self.verbose:
+			print ("Done generating.")
+		self.emissions = torch.vstack(self.emissions)
+		
+	def next(self, state, action):
+		return state + 1 
+
+	def convert(self, state):
+		pass
+
+	def available_actions(self, state):
+		actions = []
+		actions.append(0)
+		for i in range(1,self.actions_num):
+			if self.is_valid_action(i, state):
+				actions.append(i)
+
+		# if self.verbose:
+		# 	print (f"Available actions in state {state}: {actions}")
+		return actions
+
+	def step(self,action):
+		self.visitations[self.state, action] += 1
+		self.state = self.next(self.state, action)
+		return self.state
+
+	def is_valid_action(self, action, state) -> bool:
+		if action == 0 and state>0:
+			return True 
+		# elif action == 0 and state ==0:
+		# 	return False
+		else:
+			if state in self.tokens[self.unique_elements[action]]:
+				return True
+			else:
+				return False
+
+	def p_next(self, state, action):
+		probs = {min(state + 1,self.max_episode_length-1): 1}
+		#probs[state + 1] = 1
+		return probs
+
+	def get_transition_matrix(self) -> torch.Tensor:
+		if self.transition_matrix is not None:
+			return self.transition_matrix
+
+		P = torch.zeros(size=(self.states_num, self.actions_num, self.states_num), dtype=torch.float64)
+
+		for s in range(self.states_num):
+			for a in range(self.actions_num):
+				if self.is_valid_action(a, s):
+					print (f"Valid action {a} in state {s}")
+					probs = self.p_next(s, a)
+					for s_state in probs.keys():
+						P[s, a, s_state] = probs[s_state]
+		self.transition_matrix = P
+		return P
+
+
+	def reset(self) -> None:
+		self.state = self.init_state
+		self.h = 0
+
+
+if __name__ == "__main__":
+	file_path = '../../experiments/llm/mediums.txt'
+	with open(file_path, 'r') as file:
+		words_list = [line.strip() for line in file]
+	env = LLMGrid(list_of_text_tokens = [words_list,words_list], verbose=True)
---
+++ experiments/llm/run_exp.py
@@ -0,0 +1,67 @@
+import torch
+import argparse
+
+from doexpy.env.llm import LLMGrid
+from doexpy.functionals.doe_static_functionals import DesignA, DesignD
+from doexpy.mdpexplore import MdpExplore
+from doexpy.convex_solvers.frank_wolfe import FrankWolfe
+from doexpy.feedback.feedback_base import EmptyFeedback
+from doexpy.solvers.dp import DP
+from doexpy.policies.summary_policies.density_policy import DensityPolicy
+
+parser = argparse.ArgumentParser(description='Protein capacity experiment.')
+parser.add_argument('--episodes', default=10, type=int, help='Name of the file')
+parser.add_argument('--no_tokens', default=2, type=int, help='Name of the file')
+parser.add_argument('--feedback', default='markovian', type=str, help='Name of the file')
+
+args = parser.parse_args()
+
+file_path = 'dummy.txt'
+
+with open(file_path, 'r') as file:
+    words_list = [line.strip() for line in file]
+
+T = args.episodes
+env = LLMGrid(list_of_text_tokens = [words_list,words_list, words_list, words_list], verbose=True)
+
+design = DesignA(
+    env=env,
+    lambd=1.,
+    dim = 1, # this signifies the actions matter 
+)
+
+# define the convex solver
+convex_solver = FrankWolfe(env,
+                           objective=design,
+                           num_components=100,
+                           solver=DP,
+                           #step=,
+                           SummarizedPolicyType=DensityPolicy,
+                           accuracy=1e-15)
+
+# define the feedback class
+feedback = EmptyFeedback(env, design)
+
+initial_policy = False
+
+me = MdpExplore(
+    env=env,
+    objective=design,
+    convex_solver=convex_solver,
+    verbosity=3,
+    feedback=feedback,
+    general_policy='markovian'
+)
+
+val, opt_val, visits = me.run(
+    episodes=T,
+    return_visitations = True
+)
+print (visits)
+for no, episode in enumerate(visits):
+    states, actions = episode
+    print (f"Trajectory {no}:",end = ' ')
+    #print (actions)
+    for action in actions:
+        print (env.unique_elements[action],end = ', ')
+    print()
