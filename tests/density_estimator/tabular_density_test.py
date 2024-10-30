a85082ba197d240bf93292e6f2aeb32c16a892b2
jpfolch <jpfolch@outlook.com>
2024-10-30 18:58:59 +0000

    fixed density testing

--- tests/density_estimator/tabular_density_test.py
+++ tests/density_estimator/tabular_density_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from doexpy.env.env_dummy_testing import DummyTestEnv
 from doexpy.functionals.doe_adaptive_functionals import AdaptiveDesignD
 from doexpy.densities.density_estimators import TabularDensity
@@ -14,7 +15,7 @@ density_estimator = TabularDensity(env, design)
 @pytest.mark.parametrize("policy_type", ['StationaryPolicy', 'NonStationaryPolicy'])
 def test_density_oracle_single(policy_type : str):
     # test that the density oracle returns the correct density
-    p = np.array([[0., 1., 0., 0., 0.],
+    p = torch.tensor([[0., 1., 0., 0., 0.],
                 [0., 0., 1., 0., 0.],
                 [0., 0., 0., 1., 0.],
                 [0., 0., 0., 0., 1.],
@@ -35,16 +36,16 @@ def test_density_oracle_single(policy_type : str):
                     density[s, a] == 0, 'policy suggests a negative probability'
     
         # test that the density is correct
-        correct_density = np.array([[0., 1., 0., 0., 0.],
+        correct_density = torch.tensor([[0., 1., 0., 0., 0.],
                                     [0., 0., 1., 0., 0.],
                                     [0., 0., 0., 1., 0.],
                                     [0., 0., 0., 0., 1.],
                                     [0., 0., 0., 0., 7.]])
         correct_density = correct_density / 11
-        assert np.all(density == correct_density), 'density oracle returns incorrect density'
+        assert np.all(np.isclose(density, correct_density)), 'density oracle returns incorrect density'
     
     elif policy_type == 'NonStationaryPolicy':
-        ps = np.expand_dims(p, axis=0).repeat(10, axis=0)
+        ps = p.reshape(1, 5, 5).repeat(10, 1, 1)
         # test for non-stationary policy
         policy = NonStationaryPolicy(env, ps)
         density = density_estimator.density_oracle_single(policy)
@@ -60,7 +61,7 @@ def test_density_oracle_single(policy_type : str):
                         density[h, s, a] == 0, 'policy suggests a negative probability'
         
         # test that the density is correct
-        correct_density = np.array([[[0., 1., 0., 0., 0.],
+        correct_density = torch.tensor([[[0., 1., 0., 0., 0.],
                                     [0., 0., 0., 0., 0.],
                                     [0., 0., 0., 0., 0.],
                                     [0., 0., 0., 0., 0.],
@@ -120,4 +121,4 @@ def test_density_oracle_single(policy_type : str):
                                     [0., 0., 0., 0., 0.],
                                     [0., 0., 0., 0., 1.]]])
 
-        assert np.all(density == correct_density), 'density oracle returns incorrect density'
+        assert np.all(np.isclose(density, correct_density)), 'density oracle returns incorrect density'
