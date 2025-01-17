2e5f536879d5fdfd210352e6e3c7c77703bb6c90
Guy <gshacht@ethz.ch>
2025-01-17 23:34:25 +0100

    T=500, with model normalization

--- doexpy/env/llm.py
+++ doexpy/env/llm.py
@@ -231,8 +231,8 @@ def load_aesthetics_embedding(weights_path='text_weights.pth'):
         state = torch.load(weights_path, map_location=device)
         weight = state['net.0.weight'].to(device).double()
         bias = state['net.0.bias'].to(device).double()
-        return weight, bias
-        #return weight / torch.norm(weight, p=2, dim=1, keepdim=True)
+        #return weight, bias
+        return weight / torch.norm(weight, p=2, dim=1, keepdim=True), None
         
     except FileNotFoundError:
         raise FileNotFoundError(f"Could not find weights file: {weights_path}")
