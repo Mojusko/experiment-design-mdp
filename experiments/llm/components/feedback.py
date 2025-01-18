cdea75648b25671d5ef4c00de30ab18865f2a553
Guy <gshacht@ethz.ch>
2025-01-18 21:13:46 +0100

    750,75 exp, back to Orig, 3 policies

--- experiments/llm/components/feedback.py
+++ experiments/llm/components/feedback.py
@@ -91,8 +91,7 @@ class FeedbackFactory:
             estimator = KernelizedFeatures(embedding, m)
             fb = NumericalFeedback(env, design, estimator)
         else:
-            #design = MultiPolicyOrigDesignD(env=env, lambd=cfg.feedback.lambda_reg, dim=1)
-            design = MultiPolicyAggDesignD(env=env, lambd=cfg.feedback.lambda_reg, dim=1)
+            design = MultiPolicyOrigDesignD(env=env, lambd=cfg.feedback.lambda_reg, dim=1)
             likelihood = MultinomialLikelihood()
             regularizer = L2Regularizer(lam=cfg.feedback.lambda_reg)
             estimator = RegularizedMultinomialEstimator(embedding, likelihood, regularizer)
