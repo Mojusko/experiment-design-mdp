d625e6f58b6043ece5a7b61fbf0de33da847eb4b
Guy <gshacht@ethz.ch>
2025-01-20 07:18:12 +0100

    more updates

--- experiments/llm/components/feedback.py
+++ experiments/llm/components/feedback.py
@@ -75,6 +75,7 @@ class MultinomialFeedback(BaseFeedback):
                 sample_idx += 1
 
         estimator.load_data((self.env.emissions.detach().cpu(), torch.zeros(len(self.env.emissions))))
+        import ipdb; ipdb.set_trace()
         estimator.fit(trajectory_indices, labels, sum_dim=1)
 
 
