6e1d66a4387a6a0c5f33f4fd2a38d1824cbd5871
Guy <gshacht@ethz.ch>
2025-01-23 01:01:30 +0100

    no base prompt, T=500, 2000 vocab, aesthetics, adapt every 50

--- doexpy/env/llm.py
+++ doexpy/env/llm.py
@@ -295,8 +295,7 @@ def load_aesthetics_embedding(weights_path='text_weights.pth'):
         weight = state['net.0.weight'].to(device).double()
         bias = state['net.0.bias'].to(device).double()
         norm = torch.norm(weight, p=2, dim=1, keepdim=True)
-        return weight norm , bias / norm
-        #return weight / norm , bias / norm
+        return weight / norm , bias / norm
         
     except FileNotFoundError:
         raise FileNotFoundError(f"Could not find weights file: {weights_path}")
