04bc05e57f23334765bd63c5f099e4d14ea1a6dd
Guy <gshacht@ethz.ch>
2025-02-04 22:56:35 +0100

    T=500, reg=10

--- experiments/llm/components/feedback.py
+++ experiments/llm/components/feedback.py
@@ -86,7 +86,6 @@ class MultinomialFeedback(BaseFeedback):
                 ]
                 vals = torch.tensor([theta_star(ta)[0] for ta in trunc_actions])
                 probs = F.softmax(vals.detach(), dim=0)
-                import ipdb; ipdb.set_trace()
                 label_idx = torch.multinomial(probs, 1)
 
                 trajectory_indices[sample_idx, :, :] = torch.tensor(trunc_actions).T
