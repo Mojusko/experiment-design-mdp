1b17fe6cf50888b09b89fd4ee6d526e5aa46a03a
Guy <gshacht@ethz.ch>
2025-02-24 21:40:53 +0100

    uniform init

--- experiments/llm/components/solver.py
+++ experiments/llm/components/solver.py
@@ -24,9 +24,10 @@ class SolverFactory:
             random_solver = None
 
         optimized_solver = FrankWolfe(
+            step='line-search',
             env=env, objective=design,
             num_components=cfg.feedback.num_components if cfg.algorithm != 'random' else 1,
-            num_summarized_policies=num_policies, initial_policy=cfg.algorithm=='random', solver=DP,
+            num_summarized_policies=num_policies, initial_policy=cfg.algorithm=='random' or True, solver=DP,
             SummarizedPolicyType=DensityPolicy, accuracy=cfg.accuracy,
             num_rounds=cfg.feedback.get('num_rounds', -1), stationary=True
         )
