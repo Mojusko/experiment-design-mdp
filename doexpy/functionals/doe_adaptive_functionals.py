74689160eeb81fd65cf7c3dd8a7d91ae067e19b2
Guy <gshacht@ethz.ch>
2025-02-24 14:14:20 +0100

    Two Phased approach

--- doexpy/functionals/doe_adaptive_functionals.py
+++ doexpy/functionals/doe_adaptive_functionals.py
@@ -344,6 +344,7 @@ def combined_mask(current_aggregated: torch.Tensor,
     
     # Compute the history mask: all indices with nonzero visitation.
     history_mask = (history_aggregated != 0).nonzero(as_tuple=True)[0]
+    history_mask = history_mask.to(current_mask.device)
     
     # Union the two masks.
     combined = torch.cat([current_mask, history_mask])
