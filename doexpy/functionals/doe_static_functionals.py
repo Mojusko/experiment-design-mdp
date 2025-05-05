b4a1aa2af0309c4f3d9e48d62536337337201f60
Guy <gshacht@ethz.ch>
2025-05-05 13:25:53 +0200

    refactor: Adjust weights for C and A optimality in MultiPolicyOrigDesignC

--- doexpy/functionals/doe_static_functionals.py
+++ doexpy/functionals/doe_static_functionals.py
@@ -472,7 +472,7 @@ class MultiPolicyOrigDesignC(MultiPolicyOrigDesignD):
         a_optimal_value = -torch.trace(inv_z_reg) / 100
 
         # Return the weighted sum
-        return c_optimal_value + 0.5 * a_optimal_value
+        return 0.75 * c_optimal_value + 0.25 * a_optimal_value
 
     def eval_full(self, emissions, distributions, episodes):
         """
