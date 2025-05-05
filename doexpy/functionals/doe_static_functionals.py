79606834236f5efbc474aa3cc8e86c50e2a0703a
Guy <gshacht@ethz.ch>
2025-05-05 19:18:33 +0200

    fix: Revert to mean of traces for MultiPolicyOrigDesignC functional

--- doexpy/functionals/doe_static_functionals.py
+++ doexpy/functionals/doe_static_functionals.py
@@ -432,9 +432,8 @@ class MultiPolicyOrigDesignC(MultiPolicyOrigDesignD):
                 # Calculate precision in direction C_item
                 traces.append(torch.trace(torch.linalg.inv(C_item_dev @ inv_z_reg @ C_item_dev.T)))
             # Return the mean of the precisions instead of the max
-            # return torch.mean(torch.stack(traces)) # Original: Mean
-            # return torch.prod(torch.stack(traces)) # Previous: Product
-            return torch.log(torch.stack(traces)).sum() # New: Log-Product (Sum of Logs)
+            return torch.mean(torch.stack(traces)) # Original: Mean
+            #return torch.log(torch.stack(traces)).sum() # New: Log-Product (Sum of Logs)
 
         # Handle C being a single vector
         # Assume self.C is a (1, d) tensor
